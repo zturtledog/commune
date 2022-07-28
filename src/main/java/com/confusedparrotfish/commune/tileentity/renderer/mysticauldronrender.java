eddab64aac6dadac3623b3eb9339f3de9c5c239e
zturtledog <71353802+zturtledog@users.noreply.github.com>
2022-07-27 22:17:01 -0700

    RING OF BOATS

--- src/main/java/com/confusedparrotfish/commune/tileentity/renderer/mysticauldronrender.java
+++ src/main/java/com/confusedparrotfish/commune/tileentity/renderer/mysticauldronrender.java
@@ -29,10 +29,21 @@ public class mysticauldronrender extends TileEntityRenderer<mysticauldrontile> {
             // Direction direction1 = Direction.byHorizontalIndex((i + direction.getHorizontalIndex()) % 4);
             // float f = -direction1.getHorizontalAngle();
             // matrixStackIn.rotate(Vector3f.YP.rotationDegrees(f));
-            matrixStackIn.translate(0.5, 0.9375, 0.7);
+            matrixStackIn.translate(0.5, 0.9375, 0.5);
             matrixStackIn.rotate(Vector3f.XP.rotationDegrees(90.0F));
-            matrixStackIn.scale(0.375F, 0.375F, 0.375F);
-            Minecraft.getInstance().getItemRenderer().renderItem(itemstack, ItemCameraTransforms.TransformType.FIXED, combinedLightIn, combinedOverlayIn, matrixStackIn, bufferIn);
+            matrixStackIn.scale(0.2375F, 0.2375F, 0.375F);
+
+            int rots = 23;
+
+            for (int i = 0; i < rots; i++) {
+                matrixStackIn.rotate(Vector3f.ZP.rotationDegrees(360/rots));
+
+                matrixStackIn.push();
+                matrixStackIn.translate(1.3, 0, 0.01*(i%3));//0.98563
+                Minecraft.getInstance().getItemRenderer().renderItem(itemstack, ItemCameraTransforms.TransformType.FIXED, combinedLightIn, combinedOverlayIn, matrixStackIn, bufferIn);
+                matrixStackIn.pop();
+            }
+            
             matrixStackIn.pop();
          }
     }
